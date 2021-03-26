bbd13def105dc3f527697f5dd4124f3715eb7a90
eliasjanuario <eliasjanuario.w@gmail.com>
2021-03-26 11:35:14 -0300

    :rocket: Refactor challenge

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,23 @@ import {
   useRef,
   useState,
   useCallback,
+  ComponentType
 } from 'react';
+
+import { IconBaseProps } from 'react-icons'
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string
+  icon?: ComponentType<IconBaseProps>
+  placeholder?: string
+}
+
+export function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +58,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
